c9de3f98ec92c99c730df4e3b422d654f0ce6b96
Rochel <furtonrochel@gmail.com>
2021-11-14 19:32:38 +0800

    Assignment5

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblPlayerX = new System.Windows.Forms.Label();
+            this.lblPlayerO = new System.Windows.Forms.Label();
+            this.lblScoreO = new System.Windows.Forms.Label();
+            this.lblScoreX = new System.Windows.Forms.Label();
+            this.lblDraw = new System.Windows.Forms.Label();
+            this.lblScoreDraw = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RosyBrown;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.lblScoreDraw);
+            this.panel1.Controls.Add(this.lblDraw);
+            this.panel1.Controls.Add(this.lblScoreX);
+            this.panel1.Controls.Add(this.lblScoreO);
+            this.panel1.Controls.Add(this.lblPlayerO);
+            this.panel1.Controls.Add(this.lblPlayerX);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.btnReset);
+            this.panel1.Controls.Add(this.btnNewGame);
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(638, 462);
+            this.panel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flowLayoutPanel1.Controls.Add(this.btn1);
+            this.flowLayoutPanel1.Controls.Add(this.btn2);
+            this.flowLayoutPanel1.Controls.Add(this.btn3);
+            this.flowLayoutPanel1.Controls.Add(this.btn4);
+            this.flowLayoutPanel1.Controls.Add(this.btn5);
+            this.flowLayoutPanel1.Controls.Add(this.btn6);
+            this.flowLayoutPanel1.Controls.Add(this.btn7);
+            this.flowLayoutPanel1.Controls.Add(this.btn8);
+            this.flowLayoutPanel1.Controls.Add(this.btn9);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(268, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(361, 331);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn1.Font = new System.Drawing.Font("Segoe Print", 38F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.Color.Black;
+            this.btn1.Location = new System.Drawing.Point(3, 3);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(113, 103);
+            this.btn1.TabIndex = 1;
+            this.btn1.UseVisualStyleBackColor = false;
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn2.Font = new System.Drawing.Font("Segoe Print", 38F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.ForeColor = System.Drawing.Color.Black;
+            this.btn2.Location = new System.Drawing.Point(122, 3);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(113, 103);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = false;
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn3.Font = new System.Drawing.Font("Segoe Print", 38F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.ForeColor = System.Drawing.Color.Black;
+            this.btn3.Location = new System.Drawing.Point(241, 3);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(113, 103);
+            this.btn3.TabIndex = 3;
+            this.btn3.UseVisualStyleBackColor = false;
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn4.Font = new System.Drawing.Font("Segoe Print", 38F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.ForeColor = System.Drawing.Color.Black;
+            this.btn4.Location = new System.Drawing.Point(3, 112);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(113, 103);
+            this.btn4.TabIndex = 4;
+            this.btn4.UseVisualStyleBackColor = false;
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn5.Font = new System.Drawing.Font("Segoe Print", 38F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.ForeColor = System.Drawing.Color.Black;
+            this.btn5.Location = new System.Drawing.Point(122, 112);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(113, 103);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = false;
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn6.Font = new System.Drawing.Font("Segoe Print", 38F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.ForeColor = System.Drawing.Color.Black;
+            this.btn6.Location = new System.Drawing.Point(241, 112);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(113, 103);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = false;
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn7.Font = new System.Drawing.Font("Segoe Print", 38F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.ForeColor = System.Drawing.Color.Black;
+            this.btn7.Location = new System.Drawing.Point(3, 221);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(113, 103);
+            this.btn7.TabIndex = 7;
+            this.btn7.UseVisualStyleBackColor = false;
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn8.Font = new System.Drawing.Font("Segoe Print", 38F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.ForeColor = System.Drawing.Color.Black;
+            this.btn8.Location = new System.Drawing.Point(122, 221);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(113, 103);
+            this.btn8.TabIndex = 8;
+            this.btn8.UseVisualStyleBackColor = false;
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn9.Font = new System.Drawing.Font("Segoe Print", 38F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.ForeColor = System.Drawing.Color.Black;
+            this.btn9.Location = new System.Drawing.Point(241, 221);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(113, 103);
+            this.btn9.TabIndex = 9;
+            this.btn9.UseVisualStyleBackColor = false;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.Location = new System.Drawing.Point(184, 369);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(224, 58);
+            this.btnNewGame.TabIndex = 10;
+            this.btnNewGame.Text = "Start New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(486, 349);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(138, 50);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(486, 405);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(138, 50);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // lblPlayerX
+            // 
+            this.lblPlayerX.AutoSize = true;
+            this.lblPlayerX.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerX.Location = new System.Drawing.Point(-1, 37);
+            this.lblPlayerX.Name = "lblPlayerX";
+            this.lblPlayerX.Size = new System.Drawing.Size(140, 43);
+            this.lblPlayerX.TabIndex = 13;
+            this.lblPlayerX.Text = "Player X :";
+            // 
+            // lblPlayerO
+            // 
+            this.lblPlayerO.AutoSize = true;
+            this.lblPlayerO.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerO.Location = new System.Drawing.Point(-2, 85);
+            this.lblPlayerO.Name = "lblPlayerO";
+            this.lblPlayerO.Size = new System.Drawing.Size(141, 43);
+            this.lblPlayerO.TabIndex = 14;
+            this.lblPlayerO.Text = "Player O :";
+            // 
+            // lblScoreO
+            // 
+            this.lblScoreO.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblScoreO.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblScoreO.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreO.Location = new System.Drawing.Point(140, 88);
+            this.lblScoreO.Name = "lblScoreO";
+            this.lblScoreO.Size = new System.Drawing.Size(89, 35);
+            this.lblScoreO.TabIndex = 15;
+            this.lblScoreO.Text = "0";
+            this.lblScoreO.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblScoreX
+            // 
+            this.lblScoreX.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblScoreX.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblScoreX.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreX.Location = new System.Drawing.Point(140, 37);
+            this.lblScoreX.Name = "lblScoreX";
+            this.lblScoreX.Size = new System.Drawing.Size(89, 35);
+            this.lblScoreX.TabIndex = 16;
+            this.lblScoreX.Text = "0";
+            this.lblScoreX.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblDraw
+            // 
+            this.lblDraw.AutoSize = true;
+            this.lblDraw.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDraw.Location = new System.Drawing.Point(37, 142);
+            this.lblDraw.Name = "lblDraw";
+            this.lblDraw.Size = new System.Drawing.Size(102, 43);
+            this.lblDraw.TabIndex = 17;
+            this.lblDraw.Text = "Draw :";
+            // 
+            // lblScoreDraw
+            // 
+            this.lblScoreDraw.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblScoreDraw.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblScoreDraw.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScoreDraw.Location = new System.Drawing.Point(140, 145);
+            this.lblScoreDraw.Name = "lblScoreDraw";
+            this.lblScoreDraw.Size = new System.Drawing.Size(89, 35);
+            this.lblScoreDraw.TabIndex = 18;
+            this.lblScoreDraw.Text = "0";
+            this.lblScoreDraw.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Sienna;
+            this.ClientSize = new System.Drawing.Size(647, 470);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Label lblScoreDraw;
+        private System.Windows.Forms.Label lblDraw;
+        private System.Windows.Forms.Label lblScoreX;
+        private System.Windows.Forms.Label lblScoreO;
+        private System.Windows.Forms.Label lblPlayerO;
+        private System.Windows.Forms.Label lblPlayerX;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
